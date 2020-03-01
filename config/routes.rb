19d38de74bc3967e353a71d62c52d2bd7548f5f9
takedatsutomu <mdhd.to@gmail.com>
2020-03-01 16:14:19 +0900

    [91]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get '/home' => 'homes#top'
+  get '/home/about' => 'homes#about'
+
   resources :users,only: [:show,:index,:edit,:update]
   resources :books
   devise_for :users
-  root 'home#top'
-  get 'home/about'
+end
